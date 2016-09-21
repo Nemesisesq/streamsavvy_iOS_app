ee6d5982f67c36020a8574bb23968319db1489c0
Nem <nemesisesq@gmail.com>
2016-09-21 00:35:52 -0400

    fixed cbs all access

--- Stream Savvy/PopularShow.m
+++ Stream Savvy/PopularShow.m
@@ -31,7 +31,7 @@
 
 //////// gonna save this for now, we will put it somewhere eventually
 + (void)getPopularShowsForPage:(NSInteger)page view:(UIView *)view Success:(void (^)(NSURLSessionDataTask *task, id JSON))successBlock{
-	NSString *url = @"https://ss-master-staging.herokuapp.com/api/popular-shows?";
+	NSString *url = @"https://ss-master-staging.herokuapp.com/api/popular-shows/";
 	if (page)
 		url = [NSString stringWithFormat:@"%@page%ld", url, (long)page];
 	NSLog(@"%@\n\n\n", url);
