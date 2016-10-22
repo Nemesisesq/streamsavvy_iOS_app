cdcee93d9001464eace299dec7b1eacaeacfe55c
Nem <nemesisesq@gmail.com>
2016-10-22 01:44:23 -0400

    fixed CBS and free sources

--- Stream Savvy/EpisodeCollectionViewController.swift
+++ Stream Savvy/EpisodeCollectionViewController.swift
@@ -155,12 +155,6 @@ class EpisodeCollectionViewController: UIViewController, UICollectionViewDelegat
                 }
         }
         
-        func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-                if let cell = cell as? EpisodeViewCell {
-                        cell.linkCollectionView.reloadData()
-                        
-                }
-        }
         
         
         
