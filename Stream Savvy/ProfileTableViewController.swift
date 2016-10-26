20dd5848461055fbd454a497f46013a6df220167
Nem <nemesisesq@gmail.com>
2016-10-25 22:14:03 -0400

    commiting Auth0 integrationwith profile

--- Stream Savvy/ProfileTableViewController.swift
+++ Stream Savvy/ProfileTableViewController.swift
@@ -15,6 +15,10 @@ class ProfileTableViewController: UITableViewController {
         
         let keychain = A0SimpleKeychain(service: "Auth0")
         
+        let client = A0Lock.shared().apiClient()
+        
+        let controller = A0Lock.shared().newLockViewController()
+        
         
         @IBOutlet var emailTextField: UITextField!
         
@@ -31,6 +35,63 @@ class ProfileTableViewController: UITableViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        guard let idToken = keychain.string(forKey: "id_token") else {
+                // idToken doesn't exist, user has to enter his credentials to log in
+                // Present A0Lock Login
+                A0Lock.shared().present(controller, from: self)
+                return
+        }
+        
+        client.fetchUserProfile(withIdToken: idToken,
+                                success: { profile in
+                                        // Our idToken is still valid...
+                                        // We store the fetched user profile
+                                        self.keychain.setData(NSKeyedArchiver.archivedData(withRootObject: profile), forKey: "profile")
+                                        // ✅ At this point, you can log the user into your app,
+                                        
+                                        self.emailTextField.text = profile.email
+                                        self.nicknameTextField.text = profile.nickname
+                                        
+                                        self.profileImageView.sd_setImage(with: profile.picture)
+                                        return
+                                        
+                                        
+                                        
+                                        
+                },
+                                failure: { error in
+                                        // ⚠️ idToken has expired or is no longer valid
+                                        // See step 4
+                                        
+                                        guard let refreshToken = self.keychain.string(forKey: "refresh_token") else {
+                                                self.keychain.clearAll()
+                                                
+                                                // ⛔️ At this point, you should ask the user to enter his credentials again!
+                                                A0Lock.shared().present(self.controller, from: self)
+                                                return
+                                        }
+                                        
+                                        self.client.fetchNewIdToken(withRefreshToken: refreshToken,
+                                                               parameters: nil,
+                                                               success: {newToken in
+                                                                
+                                                                self.keychain.setString(newToken.idToken, forKey: "id_token")
+                                                                
+                                                                // ✅ At this point, you can log the user into your app, by navigating to the corresponding screen
+                                                },
+                                                               failure: { error in
+                                                                
+                                                                // refreshToken is no longer valid (e.g. it has been revoked)
+                                                                // Cleaning stored values since they are no longer valid
+                                                                
+                                                                self.keychain.clearAll()
+                                                                
+                                                                // ⛔️ At this point, you should ask the user to enter his credentials again!
+                                                                
+                                        })
+                                        
+                                        
+        })
     }
 
     override func didReceiveMemoryWarning() {
