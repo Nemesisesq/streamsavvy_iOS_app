b3ac0be556ee2a6bb89c05593495d20295da9dc3
Nem <nemesisesq@gmail.com>
2016-10-28 16:34:52 -0400

    mosiac layout styled

--- Stream Savvy/MozaicGuideViewController.swift
+++ Stream Savvy/MozaicGuideViewController.swift
@@ -11,171 +11,170 @@ import ADMozaicCollectionViewLayout
 import PromiseKit
 
 enum ADMozaikLayoutType {
-        case portrait
-        case landscape
+    case portrait
+    case landscape
 }
 
 class MozaicCollectionViewController: PopularShowObjectiveCViewController, ADMozaikLayoutDelegate, UICollectionViewDataSource {
-        
-        
-        override var popularShows : [Any]! {
-                didSet {
-                        mozCollectionView.reloadData()
-                }
+    
+    
+    override var popularShows : [Any]! {
+        didSet {
+            mozCollectionView.reloadData()
         }
+    }
+    
+    
+    
+    //MARK: - Variables
+    
+    private var context = 0
+    
+    let columnWidth = UIScreen.main.bounds.size.width / 3
+    
+    @IBOutlet var mozCollectionView: UICollectionView!
+    
+    
+    //        lazy var refreshControl: UIRefreshControl = {
+    //                let refreshControl = UIRefreshControl()
+    //                //                refreshControl.addTarget(self, action: #selector(self.handleRefresh(_:)), for: UIControlEvents.valueChanged)
+    //
+    //                return refreshControl
+    //        }()
+    
+    
+    
+    
+    fileprivate let ADMozaikCollectionViewLayoutExampleImagesCount = 22
+    
+    fileprivate var portraitLayout: ADMozaikLayout {
+        let columns = [ADMozaikLayoutColumn(width: columnWidth), ADMozaikLayoutColumn(width: columnWidth), ADMozaikLayoutColumn(width: columnWidth),ADMozaikLayoutColumn(width: columnWidth)]
+        let layout = ADMozaikLayout(rowHeight: columnWidth, columns: columns)
+        layout.delegate = self
+        layout.minimumLineSpacing = 1
+        layout.minimumInteritemSpacing = 1
+        return layout;
+    }
+    
+    fileprivate var landscapeLayout: ADMozaikLayout {
+        let columns = [ADMozaikLayoutColumn(width: 110), ADMozaikLayoutColumn(width: 110), ADMozaikLayoutColumn(width: 111), ADMozaikLayoutColumn(width: 111), ADMozaikLayoutColumn(width: 110), ADMozaikLayoutColumn(width: 110)]
+        let layout = ADMozaikLayout(rowHeight: 110, columns: columns)
+        layout.delegate = self
+        layout.minimumLineSpacing = 1
+        layout.minimumInteritemSpacing = 1
+        return layout;
+    }
+    
+    //MARK: - Set Up
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Auth0.calledBySubclass = true
         
+        //                refreshControl?.addTarget(self, action: #selector(self.handleRefresh), for: UIControlEvents.valueChanged)
+        //                self.mozCollectionView.addSubview(refreshControl!)
         
+    }
+    
+    //        override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+    //                print("The shows have been set!")
+    //                self.mozCollectionView.reloadData()
+    //                super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+    //        }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
-        //MARK: - Variables
+        self.setCollectionViewLayout(false, ofType: UIScreen.main.bounds.width > UIScreen.main.bounds.height ? .landscape : .portrait)
+    }
+    
+    
+    func handleRefresh(refreshControl: UIRefreshControl) {
+        // Do some reloading of data and update the table view's data source
+        // Fetch more objects from a web service, for example...
         
-        private var context = 0
-        
-        let columnWidth = UIScreen.main.bounds.size.width / 4
-        
-        @IBOutlet var mozCollectionView: UICollectionView!
-        
-        
-//        lazy var refreshControl: UIRefreshControl = {
-//                let refreshControl = UIRefreshControl()
-//                //                refreshControl.addTarget(self, action: #selector(self.handleRefresh(_:)), for: UIControlEvents.valueChanged)
-//                
-//                return refreshControl
-//        }()
-        
-        
-        
-        
-        fileprivate let ADMozaikCollectionViewLayoutExampleImagesCount = 22
-        
-        fileprivate var portraitLayout: ADMozaikLayout {
-                let columns = [ADMozaikLayoutColumn(width: columnWidth), ADMozaikLayoutColumn(width: columnWidth), ADMozaikLayoutColumn(width: columnWidth),ADMozaikLayoutColumn(width: columnWidth)]
-                let layout = ADMozaikLayout(rowHeight: columnWidth, columns: columns)
-                layout.delegate = self
-                layout.minimumLineSpacing = 1
-                layout.minimumInteritemSpacing = 1
-                return layout;
+        // Simply adding an object to the data source for this example
+        reload()
+        mozCollectionView.reloadData()
+        refreshControl.endRefreshing()
+    }
+    
+    
+    //MARK: - Helpers
+    
+    fileprivate func setCollectionViewLayout(_ animated: Bool, ofType type: ADMozaikLayoutType) {
+        self.mozCollectionView?.setCollectionViewLayout(self.portraitLayout, animated: true)
+    }
+    
+    //MARK: - ADMozaikLayoutDelegate
+    
+    func collectionView(_ collectionView: UICollectionView, layout: UICollectionViewLayout, mozaikSizeForItemAtIndexPath indexPath: IndexPath) -> ADMozaikLayoutSize {
+        if indexPath.item == 0 {
+            return ADMozaikLayoutSize(numberOfColumns: 2, numberOfRows: 2)
         }
-        
-        fileprivate var landscapeLayout: ADMozaikLayout {
-                let columns = [ADMozaikLayoutColumn(width: 110), ADMozaikLayoutColumn(width: 110), ADMozaikLayoutColumn(width: 111), ADMozaikLayoutColumn(width: 111), ADMozaikLayoutColumn(width: 110), ADMozaikLayoutColumn(width: 110)]
-                let layout = ADMozaikLayout(rowHeight: 110, columns: columns)
-                layout.delegate = self
-                layout.minimumLineSpacing = 1
-                layout.minimumInteritemSpacing = 1
-                return layout;
-        }
-        
-        //MARK: - Set Up
-        
-        override func viewDidLoad() {
-                super.viewDidLoad()
-            Auth0.calledBySubclass = true
-
-//                refreshControl?.addTarget(self, action: #selector(self.handleRefresh), for: UIControlEvents.valueChanged)
-//                self.mozCollectionView.addSubview(refreshControl!)
-        
-        }
-        
-//        override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-//                print("The shows have been set!")
-//                self.mozCollectionView.reloadData()
-//                super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
-//        }
-        
-        override func viewWillAppear(_ animated: Bool) {
-                super.viewWillAppear(animated)
-                
-                self.setCollectionViewLayout(false, ofType: UIScreen.main.bounds.width > UIScreen.main.bounds.height ? .landscape : .portrait)
-        }
-        
-        
-        func handleRefresh(refreshControl: UIRefreshControl) {
-                // Do some reloading of data and update the table view's data source
-                // Fetch more objects from a web service, for example...
-                
-                // Simply adding an object to the data source for this example
-                reload()
-                mozCollectionView.reloadData()
-                refreshControl.endRefreshing()
-        }
-        
-        
-        //MARK: - Helpers
-        
-        fileprivate func setCollectionViewLayout(_ animated: Bool, ofType type: ADMozaikLayoutType) {
-                self.mozCollectionView?.setCollectionViewLayout(self.portraitLayout, animated: true)
-        }
-        
-        //MARK: - ADMozaikLayoutDelegate
-        
-        func collectionView(_ collectionView: UICollectionView, layout: UICollectionViewLayout, mozaikSizeForItemAtIndexPath indexPath: IndexPath) -> ADMozaikLayoutSize {
-                if indexPath.item == 0 {
-                        return ADMozaikLayoutSize(numberOfColumns: 1, numberOfRows: 1)
-                }
-                if indexPath.item % 10 == 0 {
-                        return ADMozaikLayoutSize(numberOfColumns: 4, numberOfRows: 1)
-                }
-                if indexPath.item % 8 == 0 {
-                        return ADMozaikLayoutSize(numberOfColumns: 3, numberOfRows: 1)
-                }
-                else if indexPath.item % 6 == 0 {
-                        return ADMozaikLayoutSize(numberOfColumns: 2, numberOfRows: 1)
-                }
-                else if indexPath.item % 4 == 0 {
-                        return ADMozaikLayoutSize(numberOfColumns: 1, numberOfRows: 3)
-                }
-                else {
-                        return ADMozaikLayoutSize(numberOfColumns: 1, numberOfRows: 1)
-                }
-        }
-        
-        //MARK: - UICollectionViewDataSource
-        
-        func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-                let cell = collectionView.cellForItem(at: indexPath) as! OnDemandCollectionViewCell
             
-            let vc = self.storyboard?.instantiateViewController(withIdentifier: "ContentDetailViewController") as! ContentDetailViewController
-            vc.show = cell.popularShow
-            self.navigationController?.pushViewController(vc, animated: true)
+        else if indexPath.item % 4 == 0 {
+            return ADMozaikLayoutSize(numberOfColumns: 3, numberOfRows: 1)
             
+        }
+        else if indexPath.item % 2 == 0 {
+            return ADMozaikLayoutSize(numberOfColumns: 1, numberOfRows: 2)
+        }
+        else if indexPath.item % 3 == 0 {
+            return ADMozaikLayoutSize(numberOfColumns: 2, numberOfRows: 1)
+        }
+        else {
+            return ADMozaikLayoutSize(numberOfColumns: 1, numberOfRows: 1)
+        }
+    }
+    
+    //MARK: - UICollectionViewDataSource
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath) as! OnDemandCollectionViewCell
+        
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "ContentDetailViewController") as! ContentDetailViewController
+        vc.show = cell.popularShow
+        self.navigationController?.pushViewController(vc, animated: true)
+        
+        
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        let show = popularShows[indexPath.row] as! PopularShow
+        let cell = cell as! OnDemandCollectionViewCell
+        cell.imgView.sd_setImage(with: URL(string : show.image_link ))
+        cell.titleLable.text = show.title
+        cell.popularShow = show
+        
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if let x = popularShows?.count {
+            return x
             
-                
         }
         
-        func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-                let show = popularShows[indexPath.row] as! PopularShow
-                let cell = cell as! OnDemandCollectionViewCell
-                cell.imgView.sd_setImage(with: URL(string : show.image_link ))
-                cell.titleLable.text = show.title
-                cell.popularShow = show
-
-        }
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ADMozaikLayoutCell", for: indexPath) as! OnDemandCollectionViewCell
         
-        func numberOfSections(in collectionView: UICollectionView) -> Int {
-                return 1
-        }
-        
-        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-                if let x = popularShows?.count {
-                        return x
-                        
-                }
-                
-                return 0
-        }
-        
-        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ADMozaikLayoutCell", for: indexPath) as! OnDemandCollectionViewCell
-                
-                               return cell
-        }
-        
-        //MARK: - Orientation
-        
-        //        override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-        //                super.viewWillTransition(to: size, with: coordinator)
-        //                coordinator.animate(alongsideTransition: nil) { context in
-        //                        self.setCollectionViewLayout(false, ofType: size.width > size.height ? .landscape : .portrait)
-        //                }
-        //        }
+        return cell
+    }
+    
+    //MARK: - Orientation
+    
+    //        override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+    //                super.viewWillTransition(to: size, with: coordinator)
+    //                coordinator.animate(alongsideTransition: nil) { context in
+    //                        self.setCollectionViewLayout(false, ofType: size.width > size.height ? .landscape : .portrait)
+    //                }
+    //        }
 }
