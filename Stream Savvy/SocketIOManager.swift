2a995e645ba0f95eb3cbc7716a5fd7555d2e6c2e
Nem <nemesisesq@gmail.com>
2016-11-18 00:51:27 -0500

    saving current state of app

---
+++ Stream Savvy/SocketIOManager.swift
@@ -0,0 +1,13 @@
+//
+//  SocketIOManager.swift
+//  Stream Savvy
+//
+//  Created by Carl Lewis on 11/17/16.
+//  Copyright © 2016 Stream Savvy. All rights reserved.
+//
+
+import UIKit
+
+class SocketIOManager: NSObject {
+
+}
