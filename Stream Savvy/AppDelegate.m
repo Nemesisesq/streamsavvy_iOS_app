78bbce9d527dab25bd527ee3c045a91699dbd23d
Nem <nemesisesq@gmail.com>
2016-11-22 23:58:00 -0500

    added optimizely and Mopub

--- Stream Savvy/AppDelegate.m
+++ Stream Savvy/AppDelegate.m
@@ -14,10 +14,15 @@
 #import <Fabric/Fabric.h>
 #import <Crashlytics/Crashlytics.h>
 #import <TwitterKit/TwitterKit.h>
+#import <PubNub/PubNub.h>
+#import <MoPub/MoPub.h>
 
 
 
 @interface AppDelegate ()
+
+@property (nonatomic, strong) PubNub *client;
+
 
 @end
 
@@ -55,10 +60,48 @@
     
     
     
-    [Fabric with:@[[Crashlytics class], [Twitter class]]];
+    [Fabric with:@[[Twitter class], [PubNub class], [Crashlytics class], [MoPub class]]];
+    
+    self.client = [PubNub client];
+
+    UIUserNotificationType types = (UIUserNotificationTypeBadge | UIUserNotificationTypeSound |
+                                    UIUserNotificationTypeAlert);
+    UIUserNotificationSettings *mySettings = [UIUserNotificationSettings settingsForTypes:types categories:nil];
+    
+    // In iOS 8, this is when the user receives a system prompt for notifications in your app
+    [[UIApplication sharedApplication] registerUserNotificationSettings:mySettings];
+    [[UIApplication sharedApplication] registerForRemoteNotifications];
+
     
     return YES;
 }
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    
+    [self.client addPushNotificationsOnChannels:@[@"announcements"] withDevicePushToken:deviceToken
+                                  andCompletion:^(PNAcknowledgmentStatus *status) {
+                                      
+                                      // Check whether request successfully completed or not.
+                                      if (!status.isError) {
+                                          
+                                          // Handle successful push notification enabling on passed channels.
+                                      }
+                                      // Request processing failed.
+                                      else {
+                                          
+                                          // Handle modification error. Check 'category' property to find out possible issue because
+                                          // of which request did fail.
+                                          //
+                                          // Request can be resent using: [status retry];
+                                      }
+                                  }];
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    
+    // Something went wrong and you better to look into error description.
+}
+
 
 - (void)harpyUserDidSkipVersion{
     UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:[NSBundle mainBundle]];
